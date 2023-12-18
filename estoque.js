14ce2dc78aee7d4beab7dbd004e6a8ce15ac9a46
Joao Pedro Reis <joaoreis.pedro@gmail.com>
2023-12-17 21:13:42 -0300

    ajuste no estoque

--- estoque.js
+++ estoque.js
@@ -1,16 +1,15 @@
-let estoque ={
-    'joao':[
-        {'tipo': 'maca', 'quantidade': 1},
-  ],
-    'maria':[
-        {'tipo': 'maca', 'quantidade': 2},
-  ],
-  };
-  
-  function getEstoque() {
+let estoque = {
+    'joao': [
+        { 'tipo': 'maca', 'quantidade': 1 },
+    ],
+    'maria': [
+        { 'tipo': 'maca', 'quantidade': 2 },
+    ],
+};
+
+function getEstoque() {
     return structuredClone(estoque);
-  }
-  
+}
   
   function transacaoNoEstoque(origem, destino, tipo, quantidade) {
     if (!estoque[origem] && origem !== "pomar") {
